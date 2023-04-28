f6ca0ade2878d08d455d4b040ccfb1a62982d347
Wagner Herculano <wagherculano@hotmail.com>
2023-04-28 15:04:09 -0300

    Removida utilizacao de Enums para utilizacao de Interfaces

--- loja/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
+++ loja/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
@@ -5,11 +5,7 @@ import br.com.alura.loja.orcamento.Orcamento;
 import java.math.BigDecimal;
 
 public class CalculadoraDeImpostos {
-    public BigDecimal cacular(Orcamento orcamento, TipoImposto tipoImposto){
-        return switch (tipoImposto) {
-            case ISS -> orcamento.getValor().multiply(new BigDecimal("0.06"));
-            case ICMS -> orcamento.getValor().multiply(new BigDecimal("0.1"));
-            default -> BigDecimal.ZERO;
-        };
+    public BigDecimal cacular(Orcamento orcamento, Imposto imposto) {
+        return imposto.calcular(orcamento);
     }
 }
