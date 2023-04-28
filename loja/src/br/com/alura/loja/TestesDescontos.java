90ddcbb12ecf62bafe10d2aacbdb5602e7fc2ea2
Wagner Herculano <wagherculano@hotmail.com>
2023-04-28 15:52:32 -0300

    Adicionado metodo main para testar calculo de descontos.

---
+++ loja/src/br/com/alura/loja/TestesDescontos.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TestesDescontos {
+    public static void main(String[] args) {
+        Orcamento orcamento1 = new Orcamento(new BigDecimal("1000"), 5);
+        Orcamento orcamento2 = new Orcamento(new BigDecimal("200"), 6);
+        CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+        System.out.println(calculadora.calcular(orcamento1));
+        System.out.println(calculadora.calcular(orcamento2));
+    }
+}
