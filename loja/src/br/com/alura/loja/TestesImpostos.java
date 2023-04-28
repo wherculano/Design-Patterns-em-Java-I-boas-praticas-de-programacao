26a6fa6e839db4b9b25d7bde2edd57df42b122c0
Wagner Herculano <wagherculano@hotmail.com>
2023-04-28 15:24:41 -0300

    Corrigido nome do metodo

--- loja/src/br/com/alura/loja/TestesImpostos.java
+++ loja/src/br/com/alura/loja/TestesImpostos.java
@@ -9,9 +9,9 @@ import java.math.BigDecimal;
 
 public class TestesImpostos {
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-        System.out.println(calculadora.cacular(orcamento, new ISS()));
-        System.out.println(calculadora.cacular(orcamento, new ICMS()));
+        System.out.println(calculadora.calcular(orcamento, new ISS()));
+        System.out.println(calculadora.calcular(orcamento, new ICMS()));
     }
 }
--- loja/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
+++ loja/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
@@ -5,7 +5,7 @@ import br.com.alura.loja.orcamento.Orcamento;
 import java.math.BigDecimal;
 
 public class CalculadoraDeImpostos {
-    public BigDecimal cacular(Orcamento orcamento, Imposto imposto) {
+    public BigDecimal calcular(Orcamento orcamento, Imposto imposto) {
         return imposto.calcular(orcamento);
     }
 }
