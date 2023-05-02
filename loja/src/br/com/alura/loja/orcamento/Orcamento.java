6a319dcb51d5a0189785dd772b84a669b416fa99
Wagner Herculano <wagherculano@hotmail.com>
2023-05-02 11:07:11 -0300

    Aplicado o pattern State para validacao das possiveis situacoes/estados de um orcamento.

--- loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,14 +1,19 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
     private BigDecimal valor;
     private int quantidadeItens;
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
     }
 
     public BigDecimal getValor() {
@@ -17,5 +22,30 @@ public class Orcamento {
 
     public int getQuantidadeItens() {
         return quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar() {
+        this.situacao.finalizar(this);
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
     }
 }
